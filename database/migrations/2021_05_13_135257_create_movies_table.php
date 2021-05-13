a99b51ad32eca0bae3c9a03ca101fb5ad0f6e3cb
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-13 16:12:01 +0200

    Laravel installato

---
+++ database/migrations/2021_05_13_135257_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Title', 50);
+            $table->string('Genre', 50);
+            $table->string('Director', 50);
+            $table->string('Plot', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
